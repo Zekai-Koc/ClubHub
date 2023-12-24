5ee92a19c4d30102a6f80412a53e76637ab61ae4
Zekai Koç <zekaikoc@gmail.com>
2023-12-24 09:04:20 +0100

    db connection updated.

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 import dotenv from "dotenv";
 dotenv.config();
 import app from "./app.js";
+import mongoose from "mongoose";
+
+let DB = process.env.MONGODB_DATABASE.replace(
+   "<PASSWORD>",
+   process.env.MONGODB_USER_PASS
+);
+DB = DB.replace("<USERNAME>", process.env.MONGODB_USER_NAME);
+
+mongoose
+   .connect(DB)
+   .then(() => {
+      console.log("DB connection successful.");
+   })
+   .catch(() => console.log(`DB connection error! Check DB credentials.`));
 
 const port = process.env.PORT || 9999;
-
-// app.get("/", function (req, res) {
-//    res.send("Hello World");
-// });
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
