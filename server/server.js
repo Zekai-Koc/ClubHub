aff30fba0afc63ce59768748b0ba16567343f981
Zekai Koç <zekaikoc@gmail.com>
2023-12-24 08:36:11 +0100

    env file created.

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 
 const app = express();
+
+const port = process.env.PORT || 9999;
 
 app.get("/", function (req, res) {
    res.send("Hello World");
 });
 
-app.listen(8000, () => console.log(`Listening...`));
+app.listen(port, () => console.log(`Listening on port: ${port}`));
