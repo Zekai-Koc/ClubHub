5c06a9ca451c47ffb351189b2101bd612d2c6ee8
Zekai Koç <zekaikoc@gmail.com>
2023-12-24 08:47:14 +0100

    app created.

--- server/server.js
+++ server/server.js
@@ -1,13 +1,11 @@
 import dotenv from "dotenv";
 dotenv.config();
-import express from "express";
-
-const app = express();
+import app from "./app.js";
 
 const port = process.env.PORT || 9999;
 
-app.get("/", function (req, res) {
-   res.send("Hello World");
-});
+// app.get("/", function (req, res) {
+//    res.send("Hello World");
+// });
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
