e58fee3c7aa1cc1200b0da28c1f13460ff30212d
Zekai Koç <zekaikoc@gmail.com>
2023-12-28 08:42:50 +0100

    logo added.

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Footer.css";
+import logo from "../../assets/clubhub_logo_tp.png";
 
 const Footer = () => {
    return (
       <div className="footer-container">
-         <p>logo</p>
+         <img className="logo-image" src={logo} alt="logo" />
          <p>description</p>
+         <img className="logo-image" src={logo} alt="logo" />
       </div>
    );
 };
--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Header.css";
+import logo from "../../assets/clubhub_logo_tp.png";
 
 const Header = () => {
    return (
       <div className="header-container">
-         <p>logo</p>
+         <img className="logo-image" src={logo} alt="logo" />
          <p>navigation</p>
          <p>user login</p>
       </div>
